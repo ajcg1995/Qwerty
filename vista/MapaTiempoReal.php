a002c09d86b78e4d4c82758a345121be981d8358
teo19 <teo19@Milena>
2018-05-01 00:09:14 -0500

    Nuevas modif

---
+++ vista/MapaTiempoReal.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body> 
+        <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d15720.509971110643!2d-84.1382365!3d9.9233386!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2scr!4v1525150969450" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
