dcd283c8151d9796b493c622b2364aced0be28d4
Andrey <Andrey@DESKTOP-VEEUK3H>
2018-05-01 13:38:17 -0600

    Chino

--- bd/ProcedimientosAdministracionRutas.php
+++ bd/ProcedimientosAdministracionRutas.php
@@ -1,6 +1,7 @@
 <?php
 require_once '../bd/Conexion.php';
-require_once '../modelo/ruta.php';
+require_once '../modelo/Ruta.php';
+require_once '../modelo/Empresa.php';
 
 
 function listarRutas($idEmpresa){
@@ -26,8 +27,6 @@ function listarRutas($idEmpresa){
         $instancia->cerrar($conn);
     }
     return $filas;
-    
-    
 }
 
 function insertarRutas($idEmpresa,$nombreRuta,$gps,$imgHorario,$rutaEstatus,$horario){
@@ -72,14 +71,34 @@ function buscarRutas($idRuta){
     while ($fila = $resultado->fetch_assoc()) {
         $idPermiso = $fila['idPermiso'];
         $nombrePermiso = $fila['nombrePermiso'];
-        $filas[] = new Permiso($idPermiso, $nombrePermiso);
+        $idEmpresa = new Empresa($fila);
+        $nombreRuta = $fila['nombreRuta'];
+        $gps = $fila['gps'];
+        $imgHorario = $fila['imgHorario'];
+        $rutaEstatus = $fila['rutaEstatus'];
+        $horario = $fila['horario'];   
+        $filas[] = new Permiso($idRuta, $nombreRuta, $gps, $imgHorario, $horario, $estado,$idEmpresa);
     }
     if ($conn) {
         $instancia->cerrar($conn);
     }
-    return $filas;
-    
-    
+    return $filas;  
+}
+
+
+function modificarRuta($idRuta,$idEmpresa,$nombreRuta,$gps,$imgHorario,$rutaEstatus,$horario){
+    $instancia = Conexion::obtenerInstancia();
+    $conn = $instancia->obtenerConexion();
+    $query = "call PAmodificarRuta('$idRuta','$idEmpresa','$nombreRuta','$gps','$imgHorario','$rutaEstatus','$horario')";
+    $resultado = $conn->query($query);
+    if (!$resultado) {
+        echo -1;
+    } else {
+        echo '';
+    }
+    if ($conn) {
+        $instancia->cerrar($conn);
+    }    
 }
 
    
